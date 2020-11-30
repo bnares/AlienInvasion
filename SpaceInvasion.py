ef5f77c84a64523a7eb6d80f4543c52475e6ad39
Piotr Ostrouch <piotr.ostrouch@gmail.com>
2020-11-30 13:01:38 +0100

    Pygame window visible

---
+++ SpaceInvasion.py
@@ -0,0 +1,24 @@
+import pygame
+import sys
+
+class AlienInvasion():
+
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((1200,800))
+        pygame.display.set_caption(("ALIEN INVASION"))
+
+
+    def run_Game(self):
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    ai = AlienInvasion()
+    ai.run_Game()
+
