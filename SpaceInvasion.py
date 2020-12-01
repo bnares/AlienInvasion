f9a5d54a268e0c816a4523e9b096c4e920b454ee
Piotr Ostrouch <piotr.ostrouch@gmail.com>
2020-12-01 09:23:11 +0100

    nowe pliki ognorowane plus plig glowny gry

--- SpaceInvasion.py
+++ SpaceInvasion.py
@@ -1,12 +1,19 @@
 import pygame
 import sys
+from settings import Settings
+from ship import Ship
+
 
 class AlienInvasion():
 
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((1200,800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.ship = Ship(self)
         pygame.display.set_caption(("ALIEN INVASION"))
+
+
 
 
     def run_Game(self):
@@ -16,7 +23,11 @@ class AlienInvasion():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-        pygame.display.flip()
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+            pygame.display.flip()
+
+
 
 if __name__ == "__main__":
     ai = AlienInvasion()
