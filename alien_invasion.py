be0babbbd1a6e9845c21fe95542a1926c7f725ab
Piotr Ostrouch <piotr.ostrouch@gmail.com>
2020-12-04 20:33:26 +0100

    nowe opcje

---
+++ alien_invasion.py
@@ -0,0 +1,56 @@
+import sys
+import pygame
+import settings
+import ship
+
+
+class AlienInvasion():
+
+    def __init__(self):
+        pygame.init()
+        self.settings = settings.Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_height, self.settings.screen_width))
+        pygame.display.set_caption("Alien Invasion")
+
+        self.ship = ship.Ship(self)
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+
+                elif event.key == pygame.K_q:
+                    sys.exit()
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
+
+
+
+    def run_game(self):
+
+        while True:
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+
+
+
+
+ai = AlienInvasion()
+ai.run_game()
--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
 class Settings():
+
     def __init__(self):
-        self.screen_width = 1000
-        self.screen_height = 600
+        self.screen_width = 600
+        self.screen_height = 1000
         self.bg_color = (230,230,230)
-        self.ship_speed = 1.5
+
