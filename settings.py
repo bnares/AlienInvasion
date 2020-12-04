9812fee6b308972075ebc83681910e7e48347428
Piotr Ostrouch <piotr.ostrouch@gmail.com>
2020-12-04 22:15:47 +0100

    limiting range

--- settings.py
+++ settings.py
@@ -4,4 +4,5 @@ class Settings():
         self.screen_width = 600
         self.screen_height = 1000
         self.bg_color = (230,230,230)
+        self.ship_speed = 1.5
 
--- ship.py
+++ ship.py
@@ -15,12 +15,17 @@ class Ship():
         self.moving_right = False
         self.moving_left = False
 
+        self.settings = ai_game.settings
+        self.x = float(self.rect.x)
+
 
     def update(self):
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x = self.rect.x - 1
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left >0:
+            self.x -= self.settings.ship_speed
+
+        self.rect.x = self.x
 
 
 
