cf2bb85d7b7807083ce150563d05843e2f52c44f
Piotr Ostrouch <piotr.ostrouch@gmail.com>
2020-12-11 12:35:31 +0100

    q to quit

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,26 +14,34 @@ class AlienInvasion():
 
         self.ship = ship.Ship(self)
 
+
+    def _key_down(self, event):
+            if event.key == pygame.K_RIGHT:
+                self.ship.moving_right = True
+            elif event.key == pygame.K_LEFT:
+                self.ship.moving_left = True
+            elif event.key == pygame.K_q:
+                sys.exit()
+
+
+    def _key_up(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
     def _check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
+                self._key_down(event)
 
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
 
-                elif event.key == pygame.K_q:
-                    sys.exit()
+
 
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
-
+                self._key_up(event)
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
