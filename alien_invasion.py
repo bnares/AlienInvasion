12e63d391bcdbd5ae24f12e2383da207a303dcc3
Piotr Ostrouch <piotr.ostrouch@gmail.com>
2020-12-11 12:47:09 +0100

    full screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,6 +14,10 @@ class AlienInvasion():
 
         self.ship = ship.Ship(self)
 
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+
 
     def _key_down(self, event):
             if event.key == pygame.K_RIGHT:
