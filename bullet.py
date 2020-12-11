0b0eb1e04b2d7c2021ab112354fd531173e9f226
Piotr Ostrouch <piotr.ostrouch@gmail.com>
2020-12-11 15:21:01 +0100

    bullet file

---
+++ bullet.py
@@ -0,0 +1,30 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Bullet(Sprite):
+    def __init__(self, ai_game):
+        """Create bullet objest at the ship current position"""
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.color = self.settings.bullet_color
+
+        #create a bulllet rect at (0,0) and then set correct position
+        self.rect = pygame.Rect(0,0,self.settings.bullet_width, self.settings.bullet_height)
+        self.rect.midtop = ai_game.ship.rect.midtop
+
+        #store the bullet's position as decimal value
+
+        self.y = float(self.rect.y)
+
+    def update(self):
+        #move the bullet up to the screen
+        self.y -= self.settings.bullet_speed
+
+        #upadate the rect position
+        self.rect.y = self.y
+
+
+    def draw_bullet(self):
+        #Draw the bullet to the screen
+        pygame.draw.rect(self.screen, self.color, self.rect)
